2695db1dc907f276e1106f094a47e0b69d6c4ea1
Farhan Fauzan Abdurrahman <43526521+farhanfauzanabdurrahman1128@users.noreply.github.com>
2018-10-16 16:58:40 +0700

    Add files via upload

---
+++ lihatdata.php
@@ -0,0 +1,47 @@
+<?php
+    $host   = 'localhost';
+    $user   = 'root';
+    $pass   = '';
+    $dbName = 'mahasiswa';
+
+    $conn = mysqli_connect($host, $user, $pass, $dbName);
+
+    if (!$conn) {
+        die("Gagal terhubung dengan database: " . mysqli_connect_error());
+    }
+$query = mysqli_query($conn,"SELECT * FROM datamahasiswa");
+?>
+<form action="" method="post">
+    <table border="1" cellpadding="0" cellspacing="0">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Nim</th>
+            <th>Jenis Kelamin</th>
+            <th>Program Studi</th>
+            <th>Fakultas</th>
+            <th>Asal</th>
+            <th>Moto Hidup</th>
+        </tr>
+        <?php if(mysqli_num_rows($query)>0){ 
+        
+            $no = 1;
+            while($data = mysqli_fetch_array($query)){
+        ?>
+        <tr>
+            <td><?php echo $no ?></td>
+            <td><?php echo $data["nama"];?></td>
+            <td><?php echo $data["nim"];?></td>
+            <td><?php echo $data["jenisKel"];?></td>
+            <td><?php echo $data["prodi"];?></td>
+            <td><?php echo $data["fakultas"];?></td>
+            <td><?php echo $data["asal"];?></td>
+            <td><?php echo $data["motohidup"];?></td>
+        </tr>
+        <?php $no++; } ?>
+        <?php } ?>
+    </table>
+</form>
+
+
+<a href="form.php">Input Data Baru
