ca4b28e8377e4ea174663df0d87154a39e1bac87
Farhan Fauzan Abdurrahman <43526521+farhanfauzanabdurrahman1128@users.noreply.github.com>
2018-10-16 17:13:22 +0700

    Update lihatdata.php

--- lihatdata.php
+++ lihatdata.php
@@ -10,6 +10,20 @@
         die("Gagal terhubung dengan database: " . mysqli_connect_error());
     }
 $query = mysqli_query($conn,"SELECT * FROM datamahasiswa");
+
+$perintah = "SELECT * FROM mahasiswa WHERE nim like '%$nim%'"
+    $hasil = mysqli_query($perintah);
+    $jumlah = mysql_num_rows($hasil);
+    if($jumlah >0){
+    echo("Data yang ditemukan : $jumlah <br><hr>");
+        while($data=mysql_fetch_row($hasil)){
+        echo("Nama : $data[1] <br>");
+        echo("Asal : $data[2] <br>");
+        }
+    }
+else{
+echo("Maaf, data yang dicari tidak ada");
+}
 ?>
 <form action="" method="post">
     <table border="1" cellpadding="0" cellspacing="0">
